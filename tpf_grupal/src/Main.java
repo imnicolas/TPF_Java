fa5e8b44c982969a1003adc0e560baede58d9fdc
imnicolas <nicolas.esteban.suarez@hotmail.es>
2023-04-25 23:17:28 -0300

    add class Equipo

--- tpf_grupal/src/Main.java
+++ tpf_grupal/src/Main.java
@@ -1,5 +1,12 @@
+<<<<<<< Updated upstream:tpf_grupal/src/Main.java
+=======
+package org.example;
+
+import java.sql.SQLException;
+
+>>>>>>> Stashed changes:tpf_grupal/src/main/java/org/example/Main.java
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        Equipo.obtenerEquipo("Racing");
     }
 }
